715714aa01e8eebfce9e5fbb7f376518cd908edc
qhx0807 <820240134@qq.com>
2019-05-06 10:35:42 +0800

    217. 存在重复元素

--- contains-duplicate.js
+++ contains-duplicate.js
@@ -1,14 +1,28 @@
 /**
- * 217 存在重复元素
- *
+ * 217.存在重复元素
+ * https://leetcode-cn.com/problems/contains-duplicate/
  * @param {number[]} nums
  * @returns {boolean}
+ * @example
+ * 输入：[1,2,3,1]， 输出：true
  */
 const containsDuplicate = nums => {
-  if (nums.length <= 1) return false
-  const arr = [...new Set(nums)]
-  console.log(arr)
-  return !(arr.length === nums.length)
+  /**
+   * Set()
+   */
+  // if (nums.length <= 1) return false
+  // return !([...new Set(nums)].length === nums.length)
+
+  /**
+   * 排序
+   */
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true
+    }
+  }
+  return false
 }
 
-console.log(containsDuplicate([3, 1]))
+console.log(containsDuplicate([3, 1, 2, 4]))
