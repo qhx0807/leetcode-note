e28780737426e00e1980a9e316ee54ef2de12dcb
qhx0807 <820240134@qq.com>
2019-05-09 10:39:04 +0800

    9. 回文数

--- palindrome-number.js
+++ palindrome-number.js
@@ -1,10 +1,11 @@
 /**
- * 回文数
- * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 9. 回文数
+ * https://leetcode-cn.com/problems/palindrome-number/
  * @param {number} x
  * @return {boolean}
  * @example
  * 121 => true
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
  */
 
 const isPalindrome = (x) => x === Number(x.toString().split('').reverse().join(''))
