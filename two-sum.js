33b74d8d09abd43291805f9331d095fa136dc6dc
qhx0807 <820240134@qq.com>
2019-05-27 13:57:41 +0800

    1. 两数之和

--- two-sum.js
+++ two-sum.js
@@ -1,5 +1,6 @@
 /**
- * 两数之和
+ * 1. 两数之和
+ * https://leetcode-cn.com/problems/two-sum/
  * @param {number[]} nums
  * @param {number} target
  * @returns {number}
@@ -12,5 +13,33 @@ const twoSum = (nums, target) => {
     }
   }
 }
+console.log(twoSum([2, 7, 11, 15], 9))
 
-export default twoSum
+// 哈希表
+const twoSum1 = (nums, target) => {
+  let res = {}
+  for (let i = 0; i < nums.length; i++) {
+    res[target - nums[i]] = nums[i]
+  }
+  for (let j = 0; j < nums.length; j++) {
+    if (res[nums[j]] !== undefined) {
+      return [nums[j], res[nums[j]]]
+    }
+  }
+}
+
+console.log(twoSum1([2, 7, 11, 15], 9))
+
+// 优化
+const twoSum2 = (nums, target) => {
+  let res = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (res[nums[i]] !== undefined) {
+      return [nums[i], res[nums[i]]]
+    } else {
+      res[target - nums[i]] = nums[i]
+    }
+  }
+}
+
+console.log(twoSum2([2, 7, 11, 15], 9))
